a63884503224ff6ca6173c95cfa5b948f009b718
ifeelagood <thicccatto3@gmail.com>
2023-02-03 12:52:15 +1100

    updated config.json and added pathlib to config loader

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 import json
+import pathlib
 from types import SimpleNamespace
 
-global config
-with open("config.json", "r") as f:
-    config = json.load(f, object_hook=lambda d: SimpleNamespace(**d))
+# json decoding hook to convert json objects to SimpleNamespace objects
+_object_hook = lambda d: SimpleNamespace(**d)
+
+# load config.json
+with open("config.json") as f:
+    config = json.load(f, object_hook=_object_hook)
+
+# set config.paths.* to pathlib.Path objects
+for k, v in config.paths.__dict__.items():
+    config.paths.__dict__[k] = pathlib.Path(v)
+
